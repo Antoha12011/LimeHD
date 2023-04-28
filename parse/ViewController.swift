676bd0303129f8ae0d3eb14b5c124806e6022d96
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-04-28 21:32:03 +0300

    Step 5

--- parse/ViewController.swift
+++ parse/ViewController.swift
@@ -7,27 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-  
-    var channels = [Channels]()
-    
+class ViewController: UIViewController {
+
     @IBOutlet weak var tableView: UITableView!
+    
+    // ТУТ 100% СТАВИТСЯ ВТОРАЯ КОЛОНКА ИЗ КЛАССА NewsFeed
+    var newsData = [Channels]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getJson()
-        
-        
+        parsingJson { data in
+            self.newsData = data
+
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
-    
-    
+    func parsingJson(completion: @escaping ([Channels]) -> ()) {
+        
+        let urlString = "https://limehd.online/playlist/channels.json"
+        
+        let url = URL(string: urlString)
+        
+        guard url != nil else { return }
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url!) { data, response, error in
+            
+            if error == nil && data != nil {
+                
+                let decoder = JSONDecoder()
+                
+                do {
+                    let ParsingData = try decoder.decode(NewsFeed.self, from: data!)
+                    completion(ParsingData.channels)
+                } catch {
+                    print("Error")
+                }
+            }
+        }
+        dataTask.resume()
+        
+    }
 }
 
+    struct NewsFeed: Decodable {
+        var channels: [Channels]
+    }
 
-
-
+    struct Channels: Decodable {
+        var name_ru: String
+        var image: String
+    }
     
+
 
 
 
@@ -35,16 +71,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 // MARK: - UITableViewDelegate / UITableViewDataSource
 
 
-extension ViewController {
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return channels.count
+        return newsData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        cell.textLabel?.text = channels[indexPath.row].name_ru.capitalized
-        _ = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = newsData[indexPath.row].name_ru
         
         return cell
     }
