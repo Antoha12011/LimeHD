fba447d78afe7f05d7ca1b3acf366a0839a8db9d
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-04-28 18:57:09 +0300

    Start Project

--- parse/ViewController.swift
+++ parse/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getJson()
     }
-
-
+    
+    
+    
+    
+    // MARK: - Networking
+    
+    func getJson() {
+        
+        let urlString = "https://limehd.online/playlist/channels.json"
+        
+        let url = URL(string: urlString)
+        
+        guard url != nil else { return }
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            if error == nil && data != nil {
+                
+                let decoder = JSONDecoder()
+                
+                do {
+                    
+                    let newsFeed = try decoder.decode(NewsFeed.self, from: data!)
+        
+                    print(newsFeed)
+                }
+                catch {
+                    print("Error")
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
 }
-
