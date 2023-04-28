3dd47cd2cd4080d227882055a78c9bc1384fd969
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-04-28 21:53:28 +0300

    new

--- parse/ViewController.swift
+++ parse/ViewController.swift
@@ -43,5 +43,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
 }
 
