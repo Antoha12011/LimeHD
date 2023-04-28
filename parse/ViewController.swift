e124e836aed2a0be93da0f778a863254d0da3677
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-04-28 18:03:11 +0300

    Initial Commit

---
+++ parse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parse
+//
+//  Created by Антон Павлов on 28.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
