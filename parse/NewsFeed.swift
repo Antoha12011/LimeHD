a1110778a2d89424a258ac32165ed341dd608f64
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-04-28 19:11:55 +0300

    Step 3

--- parse/NewsFeed.swift
+++ parse/NewsFeed.swift
@@ -13,9 +13,9 @@ struct NewsFeed: Codable {
     var channels: [Channels]?
 }
 
+
 // ТУТ НУЖНО УКАЗАТЬ ВСЕ ДАННЫЕ КОТОРЫЕ ХОЧУ ПОЛУЧАТЬ ИЗ JSON
 struct Channels: Codable {
-    
     var name_ru: String
     var image: String
    // var title: String? - В ДАННЫЙ МОМЕНТ title НЕ ПОЛУЧАЕТ
--- parse/ViewController.swift
+++ parse/ViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         getJson()
+        
+        
+    }
+    
+    
+    
+}
+
+
+
+
+    
+
+
+
+
+// MARK: - UITableViewDelegate / UITableViewDataSource
+
+
+extension ViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "⚙️"
+        return cell
     }
     
 }
+
