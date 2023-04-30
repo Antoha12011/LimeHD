4c1b48c16dbe2abaed72b6fbafe89d466c4348b6
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-04-30 20:55:17 +0300

    Add segmented controller

---
+++ parse/FavoritesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesViewController.swift
+//  parse
+//
+//  Created by Антон Павлов on 30.04.2023.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
