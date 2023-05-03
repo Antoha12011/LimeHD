0969900203b9ced06ec2ab298c4380750860f01a
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-05-03 12:51:37 +0300

    add func only portrait app

--- SupportingFiles/AppDelegate.swift
+++ SupportingFiles/AppDelegate.swift
@@ -17,5 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        .portrait
+    }
 }
 
